1d6385220f3d1175940b175ba0aa546941f9672a
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-03 11:50:41 -0300

    add rota padrão

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Index from './pages/Index';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path='/' element={ <Index /> } />
       </Routes>
     </BrowserRouter>
   );
