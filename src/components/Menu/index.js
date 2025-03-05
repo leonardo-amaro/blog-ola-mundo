a3d4b2a6a16a79d0b6b34df5b229bb286626da5d
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-05 11:47:17 -0300

    adicionando sublinhado ao menu de navegação

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Menu.module.css';
 
 const Menu = () => {
+  const localizacao = useLocation();
+
   return (
     <header>
       <nav className={styles.navegacao}>
-        <Link className={styles.link} to='/'>
+        <Link 
+          className={`
+            ${styles.link}
+            ${localizacao.pathname === '/' ? styles.linkDestacado : ''}
+          `} 
+          to='/'
+        >
           Início
         </Link>
-        <Link className={styles.link} to='/sobre'>
+        <Link 
+          className={`
+            ${styles.link}
+            ${localizacao.pathname === '/sobre' ? styles.linkDestacado : ''}
+          `} 
+          to='/sobre'
+        >
           Sobre mim
         </Link>
       </nav>
