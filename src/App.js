cf715ed29799662c6e34575a194bb44b451e59ae
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-05 11:11:11 -0300

    adicionando rota /sobre e página Sobre

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Index from './pages/Index';
+import Sobre from './pages/Sobre';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={ <Index /> } />
+        <Route path='/sobre' element={ <Sobre /> } />
       </Routes>
     </BrowserRouter>
   );
