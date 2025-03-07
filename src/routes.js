e15e2bfce9d842695303ddafa142ca69c62eb6ea
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-07 11:01:25 -0300

    adicionando componente Rodape
    Criado o componente Rodape e adicionado à página da aplicação.

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Index from './pages/Index';
 import Sobre from './pages/Sobre';
 import Menu from './components/Menu';
+import Rodape from 'components/Rodape';
 
 function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ function AppRoutes() {
         <Route path='/' element={ <Index /> } />
         <Route path='/sobre' element={ <Sobre /> } />
       </Routes>
+      <Rodape />
     </BrowserRouter>
   );
 }
