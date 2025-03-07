e0afab1f10d44f3db52915bba414760dc54541a9
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-07 10:21:37 -0300

    implementando sob a lista de posts

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,12 +1,18 @@
+import styles from "./Index.module.css";
 import Banner from "components/Banner";
+import posts from "json/posts.json";
 
 export default function Index() {
   return (
     <main>
       <Banner />
-      <h1>
-        Olá, mundo!
-      </h1>
+      <ul className={styles.posts}>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.titulo}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
