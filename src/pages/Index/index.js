95d6c15a106d0b1a0e8005dfaa1956b8c7cd0379
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-07 11:19:00 -0300

    usando rotas aninhadas

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,19 +1,15 @@
 import styles from "./Index.module.css";
-import Banner from "components/Banner";
 import Post from "components/Post";
 import posts from "json/posts.json";
 
 export default function Index() {
   return (
-    <main>
-      <Banner />
-      <ul className={styles.posts}>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Post post={post} />
-          </li>
-        ))}
-      </ul>
-    </main>
+    <ul className={styles.posts}>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Post post={post} />
+        </li>
+      ))}
+    </ul>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,17 @@ import Index from './pages/Index';
 import Sobre from './pages/Sobre';
 import Menu from './components/Menu';
 import Rodape from 'components/Rodape';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path='/' element={ <Index /> } />
-        <Route path='/sobre' element={ <Sobre /> } />
+        <Route path='/' element={ <PaginaPadrao /> }>
+          <Route path='/' element={ <Index /> } />
+          <Route path='/sobre' element={ <Sobre /> } />
+        </Route>
       </Routes>
       <Rodape />
     </BrowserRouter>
