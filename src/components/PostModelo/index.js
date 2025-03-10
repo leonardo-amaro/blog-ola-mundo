0a6afa6d03e6f8492f41d7a78d5eab5597239360
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-10 10:19:44 -0300

    adicionando componente/estrutura das páginas Sobre e Posts

---
+++ src/components/PostModelo/index.js
@@ -0,0 +1,9 @@
+import styles from "./PostModelo.module.css";
+
+const PostModelo = () => {
+  return (
+    <article className={styles.postModeloContainer}></article>
+  );
+}
+
+export default PostModelo;
