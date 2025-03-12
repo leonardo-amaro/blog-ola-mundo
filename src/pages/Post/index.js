21f88c322ea53bebd2a782dabadeb67aa51c78b8
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-12 11:28:08 -0300

    resolvendo Desafio Final
    Implementando na página Post 4 cards de posts recomendados

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,18 +1,23 @@
 import "./Post.css";
+import styles from "./Post.module.css";
 import { useParams } from "react-router-dom";
 import posts from "json/posts.json";
 import PostModelo from "components/PostModelo";
 import Markdown from "react-markdown";
 import PaginaPadrao from "components/PaginaPadrao";
 import Erro404 from "pages/Erro404";
+import PostCard from "components/PostCard";
 
 export default function Post() {
   const parametros = useParams();
   const post = posts.find((post) => post.id === Number(parametros.id));
 
-  if (!post) return (
-    <Erro404 />
-  );
+  if (!post) return <Erro404 />;
+
+  const postsRecomendados = posts
+    .filter((post) => post.id !== Number(parametros.id))
+    .sort((a, b) => b.id - a.id)
+    .slice(0, 4);
 
   return (
     <PaginaPadrao>
@@ -23,6 +28,16 @@ export default function Post() {
         <div className="post-markdown-container">
           <Markdown>{post.texto}</Markdown>
         </div>
+        <h2 className={styles.tituloOutrosPosts}>
+          Outros posts que você pode gostar:
+        </h2>
+        <ul className={styles.postsRecomendados}>
+          {postsRecomendados.map((post) => (
+            <li key={post.id}>
+              <PostCard post={post} />
+            </li>
+          ))}
+        </ul>
       </PostModelo>
     </PaginaPadrao>
   );
